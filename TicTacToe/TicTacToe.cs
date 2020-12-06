4edc1614541d145619bb63876331c44d7f32b74b
Chetan <Chetan@DESKTOP-9SU09BS>
2020-12-06 11:10:35 +0530

    Add project files.

---
+++ TicTacToe/TicTacToe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class TicTacToe
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacToe Game.");
+        }
+
+    }
+}
